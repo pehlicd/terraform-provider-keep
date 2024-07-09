4adab2f0da82734c790985ada7ccb3059f43053e
pehlicd <65170388+pehlicd@users.noreply.github.com>
2024-07-09 13:58:49 +0200

    fix: fixed examples

--- examples/providers.tf
+++ examples/providers.tf
@@ -1,4 +1,5 @@
 provider "keep" {
   backend_url = "http://localhost:8080" # or use environment variable KEEP_BACKEND_URL
   api_key = "localhost" # or use environment variable KEEP_API_KEY
+  timeout = "200s" # or use environment variable KEEP_TIMEOUT
 }
--- examples/versions.tf
+++ examples/versions.tf
@@ -3,7 +3,6 @@ terraform {
 	keep = {
 	  source  = "pehlicd/keep"
 	  version = "~> 0.1.6"
-	  timeout = "5m"
 	}
   }
 }
